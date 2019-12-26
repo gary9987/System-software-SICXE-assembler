b905ffe6441c063f5eb3f67e603873dbf452fe39
Gary <s310068@shsh.tw>
2019-12-26 10:53:09 +0800

    change pass 1 output file url

--- source/main.cpp
+++ source/main.cpp
@@ -7,10 +7,10 @@ int main() {
     string in_file_url, out_file_url;
 #if defined(_WIN64) || defined(_WIN32)
     in_file_url = "..\\..\\test_sic_code\\sic_code.txt"";
-    out_file_url = "output.txt";
+    out_file_url = "pass1_result.txt";
 #else
     in_file_url = "../../test_sic_code/sic_code.txt";
-    out_file_url = "output.txt";
+    out_file_url = "pass1_result.txt";
 #endif
     PassOne ps1(in_file_url, out_file_url);
     ps1.perform();
